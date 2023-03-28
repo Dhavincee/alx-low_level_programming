047ae5d8b422c9049cbe55c8ac5b6ca886780463
Dhavincee <davidfabunmi40@gmail.com>
2023-03-28 16:38:31 -0700

    Other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: parameter to be checked
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int f;
+	int julian = 0;
+
+	for (f = 0; str[f] != 0; f++)
+		julian++;
+	if (julian % 2 == 0)
+	{
+		for (f = julian / 2; str[f] != '\0'; f++)
+			_putchar(str[f]);
+		_putchar('\n');
+	}
+}

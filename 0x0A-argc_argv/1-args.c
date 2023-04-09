42eae20f981ed099b029a25d05e9aebd646f1d88
Dhavincee <davidfabunmi40@gmail.com>
2023-04-09 09:26:03 -0700

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (EXIT_SUCCESS);
+}
